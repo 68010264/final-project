0ccd941f972c112a56c3c0a2e8285d3df8f95d3f
68010264 <68010264@kmitl.ac.th>
2025-09-18 20:23:11 +0700

    first commit

---
+++ lol.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+float number1, number2;33
+
+printf("input your number :");
+scanf("%f %f" , &number1,&number2);
+printf("your number are : %.2f,%.2f" ,number1, number2);
+return 0;
+}
